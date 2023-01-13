cd57765de1a598448bfb0a2306e35dbafe7ac781
Toth Akos <toth.akos.ferenc24725@gmail.com>
2023-01-13 04:00:56 +0100

    timer continues where it left after pressing space

--- chess_clock/MainWindow.xaml.cs
+++ chess_clock/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
+using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,31 +23,65 @@ namespace chess_clock
     /// </summary>
     public partial class MainWindow : Window
     {
+        public bool isActiveW, isActiveB;
         public int ctime;
+        public DispatcherTimer timer;
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void FoWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            whiteL.Visibility = Visibility.Collapsed;
-            whiteB.Visibility = Visibility.Collapsed;
-            blackL.Visibility = Visibility.Collapsed;
-            blackB.Visibility = Visibility.Collapsed;
             var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
             timer.Tick += timer_Tick;
             timer.Interval = new TimeSpan(0, 0, 1);
             timer.Start();
         }
-        private void timer_Tick(object sender, EventArgs e)
-        { 
 
+        public void Lodid()
+        {
+            whiteL.Visibility = Visibility.Collapsed;
+            whiteB.Visibility = Visibility.Collapsed;
+            blackL.Visibility = Visibility.Collapsed;
+            blackB.Visibility = Visibility.Collapsed;
+            tenB.Visibility = Visibility.Visible;
+            fiveB.Visibility = Visibility.Visible;
+            oneB.Visibility = Visibility.Visible;
+            idoL.Visibility = Visibility.Visible;
+        }
+
+        private void FoWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Lodid();
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (isActiveW)
+            {
+                if (ctime > 1)
+                {
+                    ctime--;
+                    whiteL.Content = string.Format("{0}:{1}", ctime / 60, ctime % 60);
+                }
+                else
+                {
+                    MessageBox.Show("Vesztett a fehér!");
+                }
+            }
+            if (isActiveB)
+            {
+                if (ctime > 1)
+                {
+                    ctime--;
+                    blackL.Content = string.Format("{0}:{1}", ctime / 60, ctime % 60);
+                }
+                else
+                {
+                    MessageBox.Show("Vesztett a fekete!");
+                }
+            }
         }
 
         private void tenB_Click(object sender, RoutedEventArgs e)
         {
-            ctime = 10;
+            ctime = 600;
             whiteB.Visibility = Visibility.Visible;
             whiteL.Visibility = Visibility.Visible;
             blackB.Visibility = Visibility.Visible;
@@ -61,7 +96,7 @@ namespace chess_clock
 
         private void fiveB_Click(object sender, RoutedEventArgs e)
         {
-            ctime = 5;
+            ctime = 300;
             whiteB.Visibility = Visibility.Visible;
             whiteL.Visibility = Visibility.Visible;
             blackB.Visibility = Visibility.Visible;
@@ -76,7 +111,7 @@ namespace chess_clock
 
         private void oneB_Click(object sender, RoutedEventArgs e)
         {
-            ctime = 1;
+            ctime = 60;
             whiteB.Visibility = Visibility.Visible;
             whiteL.Visibility = Visibility.Visible;
             blackB.Visibility = Visibility.Visible;
@@ -87,6 +122,49 @@ namespace chess_clock
             tenB.Visibility = Visibility.Collapsed;
             fiveB.Visibility = Visibility.Collapsed;
             oneB.Visibility = Visibility.Collapsed;
+        }
+
+        private void whiteB_Click(object sender, RoutedEventArgs e)
+        {
+            isActiveW = true;
+            blackB.Visibility = Visibility.Collapsed;
+            whiteB.Visibility = Visibility.Collapsed;
+        }
+
+        private void blackB_Click(object sender, RoutedEventArgs e)
+        {
+            isActiveB = true;
+            blackB.Visibility = Visibility.Collapsed;
+            whiteB.Visibility = Visibility.Collapsed;
+        }
+
+        private void closeL_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            FoWindow.Close();
+        }
+
+        private void resetL_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Lodid();
+            isActiveB= false;
+            isActiveW= false;
+        }
+
+        private void FoWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                if (isActiveB)
+                {
+                    isActiveB = false;
+                    isActiveW = true;
+                }
+                else
+                {
+                    isActiveW = false;
+                    isActiveB = true;
+                }
+            }
         }
     }
 }
